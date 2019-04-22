4b813cef9725237acc93db9c5e42d20328960792
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2019-04-22 22:09:36 +0200

    texto editado, sem acentos

--- cpp/menuApresentacao.cpp
+++ cpp/menuApresentacao.cpp
@@ -5,69 +5,69 @@ using namespace std;
 Menu de apresentação
 */
 void apresentacaoInicial(){
-    cout << "Olá, Bem vindo ao Aprendendo? Lógico!\n\n";
-    cout << "Programa Interativo de Aprendizagem e auxilio à assuntos Lógicos\n";
+    cout << "Ola, Bem vindo ao Aprendendo? Logico!\n\n";
+    cout << "Programa Interativo de Aprendizagem e auxilio a assuntos Logicos\n";
 }
 
 void menuOpcoes(){
     cout << "\nMENU: \n";
     cout << " 1 - Aprendendo a formular uma Tabela-Verdade!\n";
     cout << " 2 - Aprendendo a converter (Bin - Dec) <---> (Dec - Bin)!\n";
-    cout << " 3 - Aprendendo a simplificar expressões proposicionais!\n";
-    cout << " 4 - Aprendendo as regras básicas de proposições\n";
+    cout << " 3 - Aprendendo a simplificar expressoes proposicionais!\n";
+    cout << " 4 - Aprendendo as regras basicas de proposicoes\n";
     cout << " 0 - SAIR\n\n";
-    cout << " Digite a sua opção !\n";
+    cout << " Digite a sua opcao !\n";
 }
 
 
 void resumoTabela(){
-    cout << "\nA Tabela Verdade é um tipo lógico de tabela utilizado em lógica para considerar a validade de uma fórmula ou conferir que uma combinação está correta.\n";
-    cout << "Quer saber como se constrói uma Tabela Verdade?\n";
+    cout << "\nA Tabela Verdade eh um tipo logico de tabela utilizado em logica para considerar a validade de uma formula ou conferir que uma combinacao estah correta.\n";
+    cout << "Quer saber como se constroi uma Tabela Verdade?\n";
 
 }
 
 void infoTabela(){
     cout << "\nCOMO CONSTRUIR UMA TABELA?\n\n";
-    cout << " a) O número de linhas contidas em uma Tabela Verdade vai ser calculada por Linhas = 2^n, onde (n) é a quantidade de preposições.\n ";
-    cout << " b) Incluimos nas linhas valores correspondentes em binário de (0 ... [2^n - 1]). Exemplo: Com uma tabela de 2 preposições teremos 4 linhas, em linha[1] = 0 0, 0 em binário com 2 casas decimais. na linha[2] = 0 1, 1 em binário com 2 casas. linha[3] = 1 0 , linha[4] = 1 1 \n";
-    cout << " c) Enquanto ao número de colunas, vai depender de você, mas temos que ter obrigatoriamente n colunas + 1. Onde n é definido como o número de preposições e 1 coluna de resposta.\n"; 
-    cout << " d) Na coluna de resposta, temos o resultado das operações. Cada linha com seu respectivo bit resposta\n";
+    cout << " a) O numero de linhas contidas em uma Tabela Verdade vai ser calculada por Linhas = 2^n, onde (n) eh a quantidade de preposicoes.\n ";
+    cout << " b) Incluimos nas linhas valores correspondentes em binario de (0 ... [2^n - 1]). Exemplo: Com uma tabela de 2 preposicoes teremos 4 linhas, em linha[1] = 0 0, 0 em binario com 2 casas decimais. na linha[2] = 0 1, 1 em binario com 2 casas. linha[3] = 1 0 , linha[4] = 1 1 \n";
+    cout << " c) Enquanto ao numero de colunas, vai depender de voce, mas temos que ter obrigatoriamente n colunas + 1. Onde n eh definido como o numero de preposicoes e 1 coluna de resposta.\n"; 
+    cout << " d) Na coluna de resposta, temos o resultado das operacoes. Cada linha com seu respectivo bit resposta\n";
 }
 
 void resumoConversor(){
-    cout << "\nO conversor faz a tradução entre números de bases diferentes\n";
-    cout << "Convertendo Bin --> Dec, teremos a conversão de Binário (base 2) para Decimal (base 10)\n";
-    cout << "Convertendo Dec --> Bin, teremos a conversão de Decimal (base 10) para Binário (base 2)\n";
+    cout << "\nO conversor faz a traducao entre numeros de bases diferentes\n";
+    cout << "Convertendo Bin --> Dec, teremos a conversao de Binario (base 2) para Decimal (base 10)\n";
+    cout << "Convertendo Dec --> Bin, teremos a conversao de Decimal (base 10) para Binario (base 2)\n";
 }
 
 void infoConversor(){
-    cout << "\nCOMO CONVERTER UM NÚMERO?\n\n";
-    cout << " 1) De Binário para Décimal\n";
-    cout << "   a) Posicionalmente, o número binário tem seu bit mais considerativo da esquerda para direita. O último bit valerá menos que o primeiro!\n";
-    cout << "   b) Considerando o valor de cada bit, temos que o cada bit valerá 2^n onde n é a localidade da direita para esquerda, contada a partir do zero. Exemplo: 0 0 1 , o bit 1 irá valer 2^0. 1 0 0 , o bit 1 irá valer 2^2\n";
-    cout << "   c) O resultado do número convertido será a soma do valor de cada bit levando em conta sua considerabilidade. Exemplo: 1 0 1 1, 2^0 + 2^1 + 2^3 = 1 + 2 + 8 = 11 \n\n";
-    cout << " 2) De Decimal para Binário\n";
-    cout << "   a) O número em decimal será dividido pela base binário, ou seja 2, repetidas vezes até que se sobre 1 ou 0\n";
-    cout << "   b) O importante da divisão será o resto da divisão e seu resultado final. Exemplo: o numero 2 em decimal, 2 / 2 = 1 e 2 % 2 = 0\n";
-    cout << "   c) Chegando a ultima divisão com resto inteiro possivel, temos que reunir os restos das divisões com o ultimo resultado possivel. Exemplo: 4, 4 % 2 = 0, 4/2 = 2, como 2 ainda pode ser divisivel por 2, 2 % 2 = 0 e 2 / 2 = 1. Juntando os restos com o ultimo resultado, temos:  0 0 1\n";
-    cout << "   d) Com o resultado da divisão por 2 e seus restos concatenado, temos que apenas inverter a ordem. Exemplo anterior 0 0 1, invertemos 1 0 0, que equivale a 4\n\n";
+    cout << "\nCOMO CONVERTER UM NUMERO?\n\n";
+    cout << " 1) De Binario para Decimal\n";
+    cout << "   a) Posicionalmente, o numero binario tem seu bit mais considerativo da esquerda para direita. O ultimo bit valerah menos que o primeiro!\n";
+    cout << "   b) Considerando o valor de cada bit, temos que o cada bit valerah 2^n onde n ah a localidade da direita para esquerda, contada a partir do zero. Exemplo: 0 0 1 , o bit 1 irah valer 2^0. 1 0 0 , o bit 1 irah valer 2^2\n";
+    cout << "   c) O resultado do numero convertido serah a soma do valor de cada bit levando em conta sua considerabilidade. Exemplo: 1 0 1 1, 2^0 + 2^1 + 2^3 = 1 + 2 + 8 = 11 \n\n";
+    cout << " 2) De Decimal para Binario\n";
+    cout << "   a) O numero em decimal serah dividido pela base binario, ou seja 2, repetidas vezes ate que se sobre 1 ou 0\n";
+    cout << "   b) O importante da divisao serah o resto da divisao e seu resultado final. Exemplo: o numero 2 em decimal, 2 / 2 = 1 e 2 % 2 = 0\n";
+    cout << "   c) Chegando a ultima divisao com resto inteiro possivel, temos que reunir os restos das divisoes com o ultimo resultado possivel. Exemplo: 4, 4 % 2 = 0, 4/2 = 2, como 2 ainda pode ser divisivel por 2, 2 % 2 = 0 e 2 / 2 = 1. Juntando os restos com o ultimo resultado, temos:  0 0 1\n";
+    cout << "   d) Com o resultado da divisao por 2 e seus restos concatenado, temos que apenas inverter a ordem. Exemplo anterior 0 0 1, invertemos 1 0 0, que equivale a 4\n\n";
     
 }
 
 void resumoExpressoes(){
-    cout << "\nExpressões Proposicionais podem ser gigantes. No entanto, Existem várias regras de simplificações que podem transformar expressões gigantescas em expressões simples com a mesma equivalência lógica!\n";
+    cout << "\nExpressoes Proposicionais podem ser gigantes. No entanto, Existem várias regras de simplificacao que podem transformar expressoes gigantescas em expressões simples com a mesma equivalencia logica!\n";
 }
 
 void infoExpressoes(){
-    cout << "\nCOMO SIMPLIFICAR UMA EXPRESSÃO PROPOSICIONAL?\n\n";
-    cout << " a) Partimos da definição das proposições\n";
-    cout << " b) Assumimos uma regra que se encaixe com as proposições destinadas e aplicamos.\n";
-    cout << " c) Repetimos os passos podendo utilizar a mesma regra ou uma regra diferente, até chegarmos a uma expressão que não aceite mais nenhuma regra. Em tese será a expressão simplificada\n";
+    cout << "\nCOMO SIMPLIFICAR UMA EXPRESSAO PROPOSICIONAL?\n\n";
+    cout << " a) Partimos da definicao das proposicoes\n";
+    cout << " b) Assumimos uma regra que se encaixe com as proposicoes destinadas e aplicamos.\n";
+    cout << " c) Repetimos os passos podendo utilizar a mesma regra ou uma regra diferente, ate chegarmos a uma expressao que nao aceite mais nenhuma regra. Em tese serah a expressao simplificada\n";
 }
 
 void resumoRegras(){
-  cout << "\nRegras para simplificações de expressões lógicas\n";
-  cout << "Aqui vamos apresentar algumas regras para as simplificações de expressões lógicas complexas em expressões simples!\n";
+  cout << "\nRegras para simplificacao de expressoes logicas\n";
+  cout << "Aqui vamos apresentar algumas regras para as simplificacao de expressoes logicas complexas em expressoes simples!\n";
 }
 
 void resumoOpcoes(int opc){
@@ -85,7 +85,7 @@ void resumoOpcoes(int opc){
             resumoRegras();
             break;
         case(0):
-            cout << "ISSO É TUDO PESSOAL !!!\n ";
+            cout << "ISSO EH TUDO, PESSOAL!!!\n ";
             break;
         default:
             cout << "Desculpe? pode repetir?\n";
