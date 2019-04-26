1681414178aff2edbb543acb3ab664c7e9babe00
ManoMax <gabrielmaxcontato@gmail.com>
2019-04-26 13:33:46 -0300

    Correcao de saida Eliminacao de Equivalencia

--- cpp/simplificadorLogico.cpp
+++ cpp/simplificadorLogico.cpp
@@ -20,7 +20,7 @@ void listarOpcoes(){
 	cout << " \t10 - Dilema Construtivo\n";
 	cout << " \t11 - Exportação\n";
 	cout << " \t0 - Voltar ao menu principal\n\n";
-	cout << "Digite a sua opção!\n";
+	cout << "\tDigite a sua opção!\n";
 }
 
 void execExpressoes() {
@@ -41,93 +41,93 @@ void execExpressoes() {
     switch (options) {
         case (1):
             // Negacao
-            cout << "Digite uma Expressão duplamente negada." << endl;
+            cout << "\tDigite uma Expressão duplamente negada." << endl;
             _receive_input(literal1);
             cout << negacao(literal1);
             break;
 
         case (2):
             // Conjuncao
-            cout << "Digite os valores 1 ou 0 para a primeira Proposicao." << endl;
+            cout << "\tDigite os valores 1 ou 0 para a primeira Proposicao." << endl;
             _receive_input(literal1);
-            cout << "Digite os valores 1 ou 0 para a primeira Proposicao" << endl;
+            cout << "\tDigite os valores 1 ou 0 para a primeira Proposicao" << endl;
             _receive_input(literal2);
             cout << conjuncao(literal1, literal2);
             break;
         
         case (3):
             // Adicao Dijuntiva
-            cout << "Digite uma Expressao." << endl;
+            cout << "\tDigite uma Expressao." << endl;
             _receive_input(literal1);
             cout << adicao_disjuntiva(literal1);
             break;
 
         case (4):
             // Introducao da Equivalencia
-            cout << "Digite uma Expressao com Implicacao." << endl;
+            cout << "\tDigite uma Expressao com Implicacao." << endl;
             _receive_input(literal1);
-            cout << "Digite uma Expressao com Implicacao e Proposicoes invertidas." << endl;
+            cout << "\tDigite uma Expressao com Implicacao e Proposicoes invertidas." << endl;
             _receive_input(literal2);
             cout << introducao_de_equivalencia(literal1, literal2);
             break;
             
         case (5):
             // Eliminacao da Equivalencia
-            cout << "Digite uma Expressao com Bi-implicacao." << endl;
+            cout << "\tDigite uma Expressao com Bi-implicacao." << endl;
             _receive_input(literal1);
-            eliminacao_de_equivalencia(literal1);
+            cout << eliminacao_de_equivalencia(literal1);
             break;
         
         case (6):
             // Modus Ponens
-            cout << "Digite uma Expressao com Implicacao." << endl;
+            cout << "\tDigite uma Expressao com Implicacao." << endl;
             _receive_input(literal1);
-            cout << "Digite uma Proposicao com o primeiro valor da primeira Expressao." << endl;
+            cout << " \tDigite uma Proposicao com o primeiro valor da primeira Expressao." << endl;
             _receive_input(literal2);
             cout << modus_ponens(literal1, literal2);
             break;
         
         case (7):
             // Modus Tollens
-            cout << "Digite uma Expressao com Implicacao." << endl;
+            cout << "\tDigite uma Expressao com Implicacao." << endl;
             _receive_input(literal1);
-            cout << "Digite uma Proposicao, negando o segundo valor da primeira Expressao." << endl;
+            cout << "\tDigite uma Proposicao, negando o segundo valor da primeira Expressao." << endl;
             _receive_input(literal2);
             cout << modus_tollens(literal1, literal2);
             break;
         
         case (8):
             // Silogismo Hipotetico
-            cout << "Digite uma Expressao com Implicacao." << endl;
+            cout << "\tDigite uma Expressao com Implicacao." << endl;
             _receive_input(literal1);
-            cout << "Digite uma nova Expressao com Implicacao, e o primeiro valor sendo o segundo da primeira Expressao." << endl;
+            cout << "\tDigite uma nova Expressao com Implicacao, e o primeiro valor sendo o segundo da primeira Expressao." << endl;
             _receive_input(literal2);
             cout << silogismo_hipotetico(literal1, literal2);
             break;
         
         case (9):
             // Silogismo Disjuntivo
-            cout << "Digite uma Expressao com Disjuncao." << endl;
+            cout << "\tDigite uma Expressao com Disjuncao." << endl;
             _receive_input(literal1);
-            cout << "Digite uma Proposicao negando o segundo valor da primeira Expressao." << endl;
+            cout << "\tDigite uma Proposicao negando o segundo valor da primeira Expressao." << endl;
             _receive_input(literal2);
             cout << silogismo_disjuntivo(literal1, literal2);
             break;
         
         case (10):
             // Dilema Construtivo
-            cout << "Digite uma Expressao com Implicacao." << endl;
+            cout << "\tDigite uma Expressao com Implicacao." << endl;
             _receive_input(literal1);
-            cout << "Digite outra Expressao com Implicacao." << endl;
+            cout << "\tDigite outra Expressao com Implicacao." << endl;
             _receive_input(literal2);
-            cout << "Digite uma Expressao com Conjuncao, com os primeiros valores das Expressoes anteriores." << endl;
+            cout << "\tDigite uma Expressao com Conjuncao, com os primeiros valores das Expressoes anteriores." << endl;
             _receive_input(literal3);
             cout << dilema_construtivo(literal1, literal2, literal3);
             break;
 
         case (11):
             // Exportacao
-            cout << "Digite uma Expressao com Conjuncao, que Implica em uma Proposicao." << endl;
+            cout << "\tDigite uma Expressao com Conjuncao, que Implica em uma Proposicao." << endl;
             _receive_input(literal1);
             cout << exportacao(literal1);
             break;
@@ -163,7 +163,7 @@ basic_string<char> negacao(Literal *valor1) {
     string answer = "";
     if (valor1->unary_operator == '~' && literals_is_equals(valor1->first_value, valor1->second_value)
         && valor1->first_value->unary_operator == '~') {
-        answer = "Sua dupla negacao resulta em: " + literal_to_string(valor1->first_value);
+        answer = ("Sua dupla negacao resulta em: " + literal_to_string(valor1->first_value));
     }else
     {
         answer = "Nao eh possivel aplicar Negacao nessa expressao.";
@@ -175,7 +175,7 @@ basic_string<char> adicao_disjuntiva(Literal *valor1) {
     // adicao_disjuntiva (P) retorna P | "Qualquer Expressão"
     string answer = "";
     if (!(is_a_nil_literal(valor1))) {
-        answer = "Essa Expressao pode ser considerada igual a: " + literal_to_string(valor1) + " | Qualquer Expressão";
+        answer = ("Essa Expressao pode ser considerada igual a: " + literal_to_string(valor1) + " | Qualquer Expressão");
     }else
     {
         answer = "Nao eh possivel aplicar Adicao Disjuntiva nessa expressao.";
@@ -188,7 +188,7 @@ basic_string<char> modus_ponens(Literal *valor1, Literal *valor2) {
     string answer = "";
     if((valor1->binary_operator == '*') && literals_is_equals(valor1->first_value, valor2))
     {
-        answer = "Aplicando-se o metodo Modus Ponens, tem-se o resultado da Expressao: " + literal_to_string(valor1->second_value);
+        answer = ("Aplicando-se o metodo Modus Ponens, tem-se o resultado da Expressao: " + literal_to_string(valor1->second_value));
     } else
     {
         answer = "Nao eh possivel aplicar Modus Ponens nessa expressao.";
@@ -201,7 +201,7 @@ basic_string<char> modus_tollens(Literal *valor1, Literal *valor2) {
     string answer = "";
     if((valor1->binary_operator == '*') && (("~" + literal_to_string(valor1->second_value)) == literal_to_string(valor2)))
     {
-        answer = "Aplicando-se o metodo Modus Tollens, tem-se o resultado da Expressao: ";
+        answer = ("Aplicando-se o metodo Modus Tollens, tem-se o resultado da Expressao: ");
         answer.push_back('~');
         answer += literal_to_string(valor1->first_value);
     } else
@@ -216,7 +216,7 @@ basic_string<char> silogismo_disjuntivo(Literal *valor1, Literal *valor2) {
     string answer = "";
     if ((valor1->binary_operator == 'v') && ("~" + literal_to_string(valor1->second_value)) == literal_to_string(valor2))
     {
-        answer = "Aplicando-se o metodo Silogismo Disjuntivo, tem-se o resultado da Expressao: " + literal_to_string(valor1->first_value);
+        answer = ("Aplicando-se o metodo Silogismo Disjuntivo, tem-se o resultado da Expressao: " + literal_to_string(valor1->first_value));
     } else
     {
         answer = "Nao eh possivel aplicar Silogismo Disjuntivo nessa expressao.";
@@ -230,8 +230,8 @@ basic_string<char> silogismo_hipotetico(Literal *valor1, Literal *valor2) {
     
     if (valor1->binary_operator == '*' && valor2->binary_operator == '*' && (literals_is_equals(valor1->second_value, valor2->first_value)))
     {
-        answer =  "Aplicando-se o metodo Silogismo Hipotetico, tem-se o resultado da Expressao: " + 
-        ("(" + literal_to_string(valor1->first_value) + " -> " + literal_to_string(valor2->second_value) + ")");
+        answer =  ("Aplicando-se o metodo Silogismo Hipotetico, tem-se o resultado da Expressao: " + 
+        ("(" + literal_to_string(valor1->first_value) + " -> " + literal_to_string(valor2->second_value) + ")"));
     } else
     {
         answer = "Nao eh possivel aplicar Silogismo Hipotetico nessa expressao.";
@@ -245,9 +245,9 @@ basic_string<char> exportacao(Literal *valor1) {
 
     if (!is_a_nil_literal(valor1->first_value) && valor1->first_value->binary_operator == '^' && valor1->binary_operator == '*')
     {
-        answer =  "Aplicando-se o metodo Exportacao, tem-se o resultado da Expressao: " +
+        answer =  ("Aplicando-se o metodo Exportacao, tem-se o resultado da Expressao: " +
          (literal_to_string(valor1->first_value->first_value) + " -> (" +
-         literal_to_string(valor1->first_value->second_value) + " -> " + literal_to_string(valor1->second_value) + ")");
+         literal_to_string(valor1->first_value->second_value) + " -> " + literal_to_string(valor1->second_value) + ")"));
     } else
     {
         answer = "Nao eh possivel aplicar Exportacao nessa expressao.";
@@ -261,8 +261,8 @@ basic_string<char> dilema_construtivo(Literal *valor1, Literal *valor2, Literal 
 
     if (valor1->binary_operator == '*' && valor2->binary_operator == '*' && valor3->binary_operator == 'v')
     {
-        answer =  "Aplicando-se o metodo Dilema Construtivo, tem-se o resultado da Expressao: " + 
-        ("(" + literal_to_string(valor1->second_value) + " v " + literal_to_string(valor2->second_value) + ")");
+        answer =  ("Aplicando-se o metodo Dilema Construtivo, tem-se o resultado da Expressao: " + 
+        ("(" + literal_to_string(valor1->second_value) + " v " + literal_to_string(valor2->second_value) + ")"));
     } else
     {
         answer = "Nao eh possivel aplicar Dilema Construtivo nessa expressao.";
@@ -276,9 +276,10 @@ basic_string<char> eliminacao_de_equivalencia(Literal *valor1) {
 
     if (valor1->binary_operator == '#')
     {
-        answer =  "Aplicando-se o metodo Eliminacao de Equivalencia, tem-se o resultado da Expressao: " + 
+        
+        answer =  ("Aplicando-se o metodo Eliminacao de Equivalencia, tem-se o resultado da Expressao: " + 
         ("(" + literal_to_string(valor1->first_value) + " -> " + literal_to_string(valor1->second_value) +
-                 ") ^ (" + literal_to_string(valor1->second_value) + " -> " + literal_to_string(valor1->first_value) + ")");
+                 ") ^ (" + literal_to_string(valor1->second_value) + " -> " + literal_to_string(valor1->first_value) + ")"));
     } else
     {
         answer = "Nao eh possivel aplicar Eliminacao de Equivalencia nessa expressao.";
@@ -293,8 +294,8 @@ basic_string<char> introducao_de_equivalencia(Literal *valor1, Literal *valor2) 
     if (valor1->binary_operator == '*' && valor2->binary_operator == '*' &&
         literals_is_equals(valor1->first_value, valor2->second_value) && literals_is_equals(valor1->second_value, valor2->first_value))
     {
-        answer = "Aplicando-se o metodo Introducao de Equivalencia, tem-se o resultado da Expressao: " + 
-        ("(" + literal_to_string(valor1->first_value) + " <-> " + literal_to_string(valor2->first_value) + ")");
+        answer = ("Aplicando-se o metodo Introducao de Equivalencia, tem-se o resultado da Expressao: " + 
+        ("(" + literal_to_string(valor1->first_value) + " <-> " + literal_to_string(valor2->first_value) + ")"));
     } else
     {
         answer = "Nao eh possivel aplicar Introducao de Equivalencia nessa expressao.";
