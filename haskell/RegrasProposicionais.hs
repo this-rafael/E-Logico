388eddca1246cd0a714dbb1dbfca75a8cbeef34b
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2019-05-23 16:11:04 -0300

    alteracoes em prints

--- haskell/RegrasProposicionais.hs
+++ haskell/RegrasProposicionais.hs
@@ -81,7 +81,7 @@ module RegrasProposicionais where
     -}
     resumoElimEquivalencia :: IO()
     resumoElimEquivalencia = do
-        putStrLn " Através de uma bicondicional, podemos deduzir que existe uma implicação em ambos os termos."
+        putStrLn " Atraves de uma bicondicional, podemos deduzir que existe uma implicacao em ambos os termos."
         putStrLn " No caso, temos P <--> Q, podemos deduzir P --> Q, ou Q --> P "
     
     {-
@@ -89,7 +89,7 @@ module RegrasProposicionais where
     -}
     resumoModusPonens :: IO()
     resumoModusPonens = do
-        putStrLn " Através de uma implicação, caso a implicante seja confirmada, temos a dedução da implicância."
+        putStrLn " Atraves de uma implicaçao, caso a implicante seja confirmada, temos a deducao da implicancia."
         putStrLn " Seja P --> Q, tendo P confirmado, podemos deduzir Q."
         putStrLn " Quer saber mais sobre Modus Ponnens?"
     
@@ -98,21 +98,21 @@ module RegrasProposicionais where
     -}
     infoModusPonens :: IO()
     infoModusPonens = do 
-        putStrLn " As vezes chamada de regra do desapego, Modus Ponens é utilizado para a retirada da implicação, encurtando a fórmula lógica."
-        putStrLn " Na notação simbólica temos:"
+        putStrLn " As vezes chamada de regra do desapego, Modus Ponens eh utilizado para a retirada da implicacao, encurtando a formula logica."
+        putStrLn " Na notação simbolica temos:"
         putStrLn " P, P --> Q. logo Q"
         putStrLn " Exemplo de discurso:"
-        putStrLn " P --> Q = Se tocar O vento eu desisto do show"
+        putStrLn " P --> Q = Se tocar o vento eu desisto do show"
         putStrLn " P = Tocou o vento"
         putStrLn " Q = Eu desisti do show"
-        putStrLn " Podemos deduzir Q pois em uma implicação, só existe uma maneira de se existir falsidade, sendo V --> F. Assim numa implicação quando sabemos que existe veracidade na implicante, dependemos apenas da implicância."
+        putStrLn " Podemos deduzir Q pois em uma implicacao, só existe uma maneira de se existir falsidade, sendo V --> F. Assim numa implicacao quando sabemos que existe veracidade na implicante, dependemos apenas da implicancia."
     
     {-
     Função que retorna informação da opção Modus Tollens
     -}
     resumoModusTollens :: IO()
     resumoModusTollens = do
-        putStrLn " A partir de uma implicação, quando temos a negação do implicante, podemos deduzir a negação da implicante."
+        putStrLn " A partir de uma implicacao, quando temos a negacao do implicante, podemos deduzir a negacao da implicante."
         putStrLn " Assim P --> Q, ~Q, temos ~P"
         putStrLn " Quer saber mais sobre Modus Tollens?"
     
@@ -121,32 +121,32 @@ module RegrasProposicionais where
     -}
     infoModusTollens :: IO()
     infoModusTollens = do
-        putStrLn " Também chamado de Prova Indireta, ou negação consequente, Modus Tollens é utilizado para a retirada da implicação, encurtando a fórmula lógica."
+        putStrLn " Tambem chamado de Prova Indireta, ou negacao consequente, Modus Tollens eh utilizado para a retirada da implicacao, encurtando a formula logica."
         putStrLn " Exemplo de discurso:"
         putStrLn " P --> Q = Se tiver ingressos, então irei assistir Vingadores Ultimato."
-        putStrLn " ~Q = Não irei assistir Vingadores Ultimato."
-        putStrLn " ~P = Não tinha ingressos"
-        putStrLn " Podemos deduzir ~P pois em uma implicação quando temos a negação da implicÂncia, só temos a corretude no caso de termos a negação do implicante. assim no caso F --> V, F --> V continuam corretos"
+        putStrLn " ~Q = Nao irei assistir Vingadores Ultimato."
+        putStrLn " ~P = Nao tinha ingressos"
+        putStrLn " Podemos deduzir ~P pois em uma implicacao quando temos a negacao da implicancia, so temos a corretude no caso de termos a negacao do implicante. assim no caso F --> V, F --> V continuam corretos"
     
     {-
     Função que retorna informações da opção do Silogismo Hipotético
     -}
     resumoHipotetico :: IO()
     resumoHipotetico = do
-        putStrLn " A partir de duas implicações onde a implicância de uma seja o implicante da segunda implicação, podemos deduzir que o implicante da primeira implica a implicância da segunda"
-        putStrLn " Seja P --> Q, Q --> R então P --> R"
-        putStrLn " Quer saber mais sobre Silogismo Hipotético?"
+        putStrLn " A partir de duas implicacoes onde a implicancia de uma seja o implicante da segunda implicacao, podemos deduzir que o implicante da primeira implica a implicancia da segunda"
+        putStrLn " Seja P --> Q, Q --> R entao P --> R"
+        putStrLn " Quer saber mais sobre Silogismo Hipotetico?"
     
     {-
     Função que retorna informações adicionais do Silogismo Hipotético
     -}
     infoHipotetico :: IO()
     infoHipotetico = do
-        putStrLn " Utilizado na teoria da consequência, O silogismo hipotético não retira a implicação, apenas diminui o numero de termos e implicações presentes na fórmula."
+        putStrLn " Utilizado na teoria da consequencia, O silogismo hipotetico nao retira a implicacao, apenas diminui o numero de termos e implicacoes presentes na formula."
         putStrLn " Exemplo de discurso:"
-        putStrLn " P --> Q = Se sou aluno de Computação, então estudo programação."
-        putStrLn " Q --> R = Se estudo programação, então sei programar."
-        putStrLn " P --> R = Se sou aluno de Computação, então sei programar."
+        putStrLn " P --> Q = Se sou aluno de Computacao, então estudo programação."
+        putStrLn " Q --> R = Se estudo programacao, então sei programar."
+        putStrLn " P --> R = Se sou aluno de Computacao, então sei programar."
     
 
     {-
@@ -154,7 +154,7 @@ module RegrasProposicionais where
     -}
     resumoDisjuntivo :: IO()
     resumoDisjuntivo = do 
-        putStrLn " A partir de uma operação de disjunção quando temos a negação de um termo, podemos deduzir o termo restante."
+        putStrLn " A partir de uma operação de disjuncao quando temos a negacao de um termo, podemos deduzir o termo restante."
         putStrLn " Seja  P v Q. tendo  ~P, logo temos Q"
         putStrLn " Quer saber mais sobre Silogismo Disjuntivo?"
 
@@ -163,18 +163,18 @@ module RegrasProposicionais where
     -}
     infoDisjuntivo :: IO()
     infoDisjuntivo = do
-        putStrLn " Conhecido historicamente como Modus Tollendo Ponens, o silogismo disjuntivo é indicado apenas para a simplificação de termos."
+        putStrLn " Conhecido historicamente como Modus Tollendo Ponens, o silogismo disjuntivo eh indicado apenas para a simplificacao de termos."
         putStrLn " Exemplo de discurso:"
-        putStrLn " P v Q = Eu viajo de férias ou faço cursinho de inglês."
-        putStrLn " ~P = não viajo de férias."
-        putStrLn " Q = Então faço cursinho de inglês."
+        putStrLn " P v Q = Eu viajo de ferias ou faço cursinho de ingles."
+        putStrLn " ~P = Nao viajo de ferias."
+        putStrLn " Q = Entao faco cursinho de ingles."
     
     {-
     Função que retorna informações do Dilema Construtivo
     -}
     resumoDilemaConstrutivo :: IO()
     resumoDilemaConstrutivo = do
-        putStrLn " A partir de duas implicações totalmente distintas, se possuímos uma disjunção entre o implicante da primeira com o implicante da segunda implicação, podemos deduzir uma disjunção entre a implicância da primeira com a implicância da segunda disjunção."
+        putStrLn " A partir de duas implicacoes totalmente distintas, se possuimos uma disjuncao entre o implicante da primeira com o implicante da segunda implicacao, podemos deduzir uma disjuncao entre a implicancia da primeira com a implicancia da segunda disjuncao."
         putStrLn " Sendo assim P --> Q, R --> S. Tendo P v R, podemos deduzir Q v S"
         putStrLn " Quer saber mais sobre Silogismo Disjuntivo?"
     
@@ -183,10 +183,10 @@ module RegrasProposicionais where
     -}
     infoDilemaConstrutivo :: IO()
     infoDilemaConstrutivo = do
-        putStrLn " Também conhecido como dilema de transferência, serve para a simplificação de duas implicações resultando em uma disjunção"
+        putStrLn " Tambam conhecido como dilema de transferencia, serve para a simplificacao de duas implicacoes resultando em uma disjuncao"
         putStrLn " Exemplo de discurso:"
-        putStrLn " P --> Q = Se eu me formei, então eu tenho emprego."
-        putStrLn " R --> S = Se eu tenho dinheiro, então tenho um carro."
+        putStrLn " P --> Q = Se eu me formei, entao eu tenho emprego."
+        putStrLn " R --> S = Se eu tenho dinheiro, entao tenho um carro."
         putStrLn " P v R = Eu me formei ou tenho dinheiro."
         putStrLn " Q v S = Eu tenho um emprego ou tenho um carro."
     
@@ -195,7 +195,7 @@ module RegrasProposicionais where
     -}
     resumoExportacao :: IO()
     resumoExportacao = do 
-        putStrLn " A partir de uma implicação, se o implicante for formado por uma conjunção, podemos deduzir uma implicação do primeiro termo da conjunção implicando numa implicação do segundo termo da conjunção implicando na implicância original."
+        putStrLn " A partir de uma implicacao, se o implicante for formado por uma conjuncao, podemos deduzir uma implicacao do primeiro termo da conjuncao implicando numa implicacao do segundo termo da conjuncao implicando na implicancia original."
         putStrLn " Sendo (P ^ Q) --> S, temos P --> (Q --> S)."
     
     {-
@@ -237,7 +237,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoNegacao
-            else putStrLn "tudo bem! até mais ver em francês, Au re voir!"
+            else putStrLn " tudo bem! ate mais ver em frances, Au re voir!"
 
         | opc == 2 = do
             resumoConjuncao
@@ -245,7 +245,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoConjuncao
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 3 = do
             resumoAdicao
@@ -253,7 +253,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoAdicao
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 4 = do
             resumoIntroEquivalencia
@@ -267,7 +267,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoModusPonens
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 7 = do
             resumoModusTollens
@@ -275,7 +275,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoModusTollens
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 8 = do
             resumoHipotetico
@@ -283,7 +283,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoHipotetico
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 9 = do
             resumoDisjuntivo
@@ -291,7 +291,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoDisjuntivo
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 10 = do 
             resumoDilemaConstrutivo
@@ -299,7 +299,7 @@ module RegrasProposicionais where
             resp <- getLine
             if (valida resp) 
                 then infoDilemaConstrutivo
-            else putStr "tudo bem! até mais ver em francês, Au re voir!"
+            else putStr " tudo bem! ate mais ver em frances, Au re voir!"
         
         | opc == 11 = do
             resumoExportacao
