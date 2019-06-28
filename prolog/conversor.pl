0bfb92aec148f86450345f26f4ee9a76ddd4f5a6
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2019-06-28 11:44:59 -0300

    finalizando conversor

--- prolog/conversor.pl
+++ prolog/conversor.pl
@@ -1,5 +1,3 @@
-:- initialization(execConversor).
-
 execConversor:-
     listOptions,
     write(" >>> "),
@@ -41,24 +39,8 @@ execConversion("2"):-
     decToBin(Number, X),
     writeln(X).
 
-execConversion("3"):-
-    write("\n Digite um numero binario: "),
-    read_line_to_string(user_input, binary),
-    binaryToFloat32(Binary, X),
-    writeln(X).
-
-execConversion("4"):-
-    write("\n Digite um numero float: "),
-    read_line_to_codes(user_input, Codes),
-    string_to_atom(Codes, Atom),
-    atom_number(Atom, Number),
-    floatToBinIEEE754(Number, X),
-    writeln(X).
-
 execConversionExplanation("1"):- explanationBinToDec.
 execConversionExplanation("2"):- explanationDecToBin.
-execConversionExplanation("3"):- explanationBinaryToFloat32.
-execConversionExplanation("4"):- explanationFloatToBinIEEE754.
 execConversionExplanation(X):-
     writeln(" Desculpe-me, nao consegui te entender :(").
 
@@ -70,15 +52,11 @@ listOptions:-
 
 optionsConversionExplanation:-
     writeln("\n        1 - Explicacao sobre conversao BINARIO -> DECIMAL"),
-    writeln("        2 - Explicacao sobre conversao DECIMAL -> BINARIO"),
-    writeln("        3 - Explicacao sobre conversao BINARIO -> FLOAT"),
-    writeln("        4 - Explicacao sobre conversao FLOAT -> BINARIO\n").
+    writeln("        2 - Explicacao sobre conversao DECIMAL -> BINARIO").
 
 optionsConversion:-
     writeln("\n        1 - Converter BINARIO -> DECIMAL"),
-    writeln("        2 - Converter DECIMAL -> BINARIO"),
-    writeln("        3 - Converter BINARIO -> FLOAT"),
-    writeln("        4-  Converter FLOAT -> BINARIO").
+    writeln("        2 - Converter DECIMAL -> BINARIO").
 
 explanationBinToDec:-
     writeln("\n O sistema binario possui somente dois algarismos. Cada posicao do numero em binario tem"),
@@ -109,29 +87,6 @@ explanationDecToBin:-
     writeln(" RESULTADO = 00001110 = 14                                                                              "),
     writeln("                                                                                                        ").
 
-explanationFloatToBinIEEE754:-
-    writeln("\n A representacao de numeros de ponto flutuante em binario pelo padrao IEEE 754 tem 32 bits"),
-    writeln(" de precisao. Desses 32 bits, um eh um bit dedicado ao sinal do numero, oito bits sao"),
-    writeln(" dedicados ao expoente do numero e os outros vinte e tres bits restantes sao destinados a"),
-    writeln(" a mantissa do numero. Considere o numero -9.5 sendo convertido abaixo:\n"),
-    writeln(" 1) Bit de sinal: 1 (NEGATIVO = 1, POSITIVO = 0)"),
-    writeln(" 2) Conversao para binario: 9.5 --> 1001.1"),
-    writeln(" 3) Deslocar a virgula e 'esconder' o primeiro bit: 0011"),
-    writeln(" 4) Multiplicar por 2 elevado a quantidade de deslocamentos: 0011 x 2³"),
-    writeln(" 5) Normalizar o expoente somando com 127: 3 + 127 = 130 = 10000010"),
-    writeln(" 6) Agrupar o resultado na forma: bit sinal | expoente | mantissa"),
-    writeln(" 7) Resultado: 1 | 10000010 | 00110000000000000000000").
-
-explanationBinaryToFloat32:-
-    writeln("\n Um numero binario de 32 bits pode ser convertido em um numero decimal com sinal. Para isso"),
-    writeln(" realiza-se o inverso das operacoes aplicadas na conversao de numero float para binario."),
-    writeln(" Considere o numero 11000001000110000000000000000000 sendo convertido abaixo:\n"),
-    writeln(" 1) Subtrair 127 do expoente: 10000010 = 130 => 130 - 127 = 3"),
-    writeln(" 2) Retornar o bit escondido: 1.0011"),
-    writeln(" 3) Deslocar a virgula: 1.0011 => 1001.1"),
-    writeln(" 4) Converter para binario: 1001.1 = 9.5"),
-    writeln(" 5) Adicionar sinal (se for negativo): -9.5").
-
 decToBin(0,'0').
 decToBin(1,'1').
 decToBin(N,B):-
@@ -148,9 +103,3 @@ binaryToDec(Binary, Return):-
     X is Binary // 10,
     binaryToDec(X, Y),
     Return is 2 * Y + R.
-
-%TODO
-binaryToFloat32(X, 0.0).
-
-%TODO
-floatToBinIEEE754(X, 0).
