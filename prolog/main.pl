6fa42c0eeea42da1d934813a15db7f6bdc698829
ManoMax <gabrielmaxcontato@gmail.com>
2019-06-27 22:14:46 -0300

    simplificador_logico proLog 1.0 adicionado na Main

--- prolog/main.pl
+++ prolog/main.pl
@@ -10,8 +10,8 @@
               |
               v
 */
-%:-use_module('simplificador_logico').
 :- [truth_table_generator].
+:- [simplificador_logico].
 
 
 main :-
@@ -44,7 +44,7 @@ conversor:-
     writeln('aqui vem o conversor'), loop.
 simplificadorLogico:-
     % tty_clear,
-    writeln('aqui vem o simplificadorLogico'), loop.
+    execSimplificador(), loop.
 regrasProposicionais:-
     % tty_clear,
     writeln('aqui aparece as coisas das regrasProposicionais'), loop.
